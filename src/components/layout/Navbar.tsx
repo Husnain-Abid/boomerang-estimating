dc7d8f499c6e42c8df9d876c8d8c9d47240e75f9
Husnain-Abid <134037646+Husnain-Abid@users.noreply.github.com>
2026-07-25 16:40:59 +0500

    Update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useLocation } from "wouter";
-import { Menu, X, Target, ArrowRight, ChevronDown } from "lucide-react";
+import { Menu, X, ArrowRight, ChevronDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { SERVICES } from "@/lib/data";
 import {
